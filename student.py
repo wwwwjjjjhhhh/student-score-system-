8e46a5fae070e2d26f8f4ef1282579440af5147b
wwwwjjjjhhhh <3421869381@qq.com>
2026-09-21 09:30:15 +0800

    Merge feature-score into develop and resolve conflict

--- student.py
+++ student.py
@@ -1,5 +1,11 @@
 def show_student():
-    print("Student Management System")
+   name = input("请输入学生姓名：")
+    gender = input("请输入学生性别：")
+    score = float(input("请输入学生成绩："))
+
+    print(f"学生姓名：{name}")
+    print(f"学生性别：{gender}")
+    print(f"学生成绩：{score}")
 
 
 if __name__ == "__main__":
